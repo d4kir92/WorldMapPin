821e80b65b4a2dc3f469ab503d39c1ef796bda80
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-01-30 10:42:52 +0100

    v1.1.62

--- core.lua
+++ core.lua
@@ -179,6 +179,6 @@ WorldMapPin:UpdatePinPos()
 C_Timer.After(
 	0,
 	function()
-		WorldMapPin:SetVersion(AddonName, 134269, "1.1.61")
+		WorldMapPin:SetVersion(AddonName, 134269, "1.1.62")
 	end
 )
